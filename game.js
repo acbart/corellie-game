50f845208cd9d3b63ea03b78040f36b1474cfd7c
Austin Cory Bart <acbart@vt.edu>
2017-05-23 12:14:12 -0400

    Second example

--- game.js
+++ game.js
@@ -1,64 +1,80 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
-    game.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x2.png');
-    game.load.image('tiles2', 'assets/tilemaps/tiles/tiles2.png');
+    game.load.image('atari', 'assets/sprites/block.png');
+    game.load.image('background', 'assets/games/starstruck/background2.png');
     game.load.spritesheet('dude', 'assets/games/starstruck/dude.png', 32, 48);
 
 }
+
+var sprite;
 
 var player;
 var facing = 'left';
 var jumpTimer = 0;
 var cursors;
 var jumpButton;
-
-var map;
-var layer;
+var yAxis = p2.vec2.fromValues(0, 1);
 
 function create() {
 
+    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+
+    //  Enable p2 physics
     game.physics.startSystem(Phaser.Physics.P2JS);
 
-    game.stage.backgroundColor = '#2d2d2d';
+    game.physics.p2.gravity.y = 350;
+    game.physics.p2.world.defaultContactMaterial.friction = 0.3;
+    game.physics.p2.world.setGlobalStiffness(1e5);
 
-    map = game.add.tilemap('map');
-
-    map.addTilesetImage('ground_1x1');
-    map.addTilesetImage('walls_1x2');
-    map.addTilesetImage('tiles2');
-    
-    layer = map.createLayer('Tile Layer 1');
-
-    layer.resizeWorld();
-
-    //  Set the tiles for collision.
-    //  Do this BEFORE generating the p2 bodies below.
-    map.setCollisionBetween(1, 12);
-
-    //  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
-    //  This call returns an array of body objects which you can perform addition actions on if
-    //  required. There is also a parameter to control optimising the map build.
-    game.physics.p2.convertTilemap(map, layer);
-
-    game.physics.p2.restitution = 0.5;
-    game.physics.p2.gravity.y = 300;
-
-    player = game.add.sprite(100, 200, 'dude');
+    //  Add a sprite
+    player = game.add.sprite(200, 200, 'dude');
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('turn', [4], 20, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    //  Enable if for physics. This creates a default rectangular body.
     game.physics.p2.enable(player);
     
     player.body.fixedRotation = true;
-    // player.body.setMaterial(characterMaterial);
+    player.body.damping = 0.5;
 
-    game.camera.follow(player);
+    var spriteMaterial = game.physics.p2.createMaterial('spriteMaterial', player.body);
+    var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
+    var boxMaterial = game.physics.p2.createMaterial('worldMaterial');
+
+    //  4 trues = the 4 faces of the world in left, right, top, bottom order
+    game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
+
+    //  A stack of boxes - you'll stick to these
+    for (var i = 1; i < 4; i++)
+    {
+        var box = game.add.sprite(300, 645 - (95 * i), 'atari');
+        game.physics.p2.enable(box);
+        box.body.mass = 6;
+        // box.body.static = true;
+        box.body.setMaterial(boxMaterial);
+    }
+
+    //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
+    //  those 2 materials collide it uses the following settings.
+
+    var groundPlayerCM = game.physics.p2.createContactMaterial(spriteMaterial, worldMaterial, { friction: 0.0 });
+    var groundBoxesCM = game.physics.p2.createContactMaterial(worldMaterial, boxMaterial, { friction: 0.6 });
+
+    //  Here are some more options you can set:
+
+    // contactMaterial.friction = 0.0;     // Friction to use in the contact of these two materials.
+    // contactMaterial.restitution = 0.0;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
+    // contactMaterial.stiffness = 1e3;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+    // contactMaterial.relaxation = 0;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+    // contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+    // contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+    // contactMaterial.surfaceVelocity = 0.0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+
+    text = game.add.text(20, 20, 'move with arrow, space to jump', { fill: '#ffffff' });
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -118,25 +134,28 @@ function update() {
 
 function checkIfCanJump() {
 
-    var yAxis = p2.vec2.fromValues(0, 1);
     var result = false;
 
-    for (var i = 0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++)
+    for (var i=0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++)
     {
         var c = game.physics.p2.world.narrowphase.contactEquations[i];
 
         if (c.bodyA === player.body.data || c.bodyB === player.body.data)
         {
-            var d = p2.vec2.dot(c.normalA, yAxis); // Normal dot Y-axis
-            if (c.bodyA === player.body.data) d *= -1;
-            if (d > 0.5) result = true;
+            var d = p2.vec2.dot(c.normalA, yAxis);
+
+            if (c.bodyA === player.body.data)
+            {
+                d *= -1;
+            }
+
+            if (d > 0.5)
+            {
+                result = true;
+            }
         }
     }
     
     return result;
-
-}
-
-function render() {
 
 }
